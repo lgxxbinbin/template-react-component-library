d5920cbc5e965ee10aa5ac09ab9a27b13332aae2
tien <tiennguyen11119555@gmail.com>
2022-10-03 16:43:41 +0700

    BUild

--- dist/cjs/types/components/Tooltip/NVTooltip.d.ts
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-declare type NVTooltipProps = {
-    title: string;
-    children: React.ReactNode;
-};
-export default function NVTooltip(props: NVTooltipProps | any): JSX.Element;
-export {};
