1524f79a7c43b2d1d6e5b287e9a752f32620d51c
tien <tiennguyen11119555@gmail.com>
2022-10-03 16:37:19 +0700

    BUild

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,6 +1,21 @@
+import * as React from 'react';
+
 interface ButtonProps {
     label: string;
 }
 declare const Button: (props: ButtonProps) => JSX.Element;
 
-export { Button };
+declare type NVButtonProps = {
+    variant?: string | 'text';
+    defaultValue?: number;
+    children: React.ReactNode;
+};
+declare function NVButton(props: NVButtonProps | any): JSX.Element;
+
+declare type NVTooltipProps = {
+    title: string;
+    children: React.ReactNode;
+};
+declare function NVTooltip(props: NVTooltipProps | any): JSX.Element;
+
+export { Button, NVButton, NVTooltip };
